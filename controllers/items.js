768ab26e7a11e17da50b5b15b81db6d21fe8678d
romerorocj-ga <charliejoelromero@gmail.com>
2023-10-26 15:20:02 -0400

    List Items

--- controllers/items.js
+++ controllers/items.js
@@ -8,10 +8,12 @@ module.exports = {
 async function index(req, res) {
   const items = await Item.find({}).sort('name').populate('category').exec();
   items.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
+  console.log('Items sent:', items);
   res.json(items);
 }
 
 async function show(req, res) {
   const item = await Item.findById(req.params.id);
+  console.log('Cart sent:', cart);
   res.json(item);
 }
--- server.js
+++ server.js
@@ -1,27 +1,41 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
-const userRouter = require('./routes/users')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const userRouter = require('./routes/users');
 
-const SERVERDEVPORT = 4741
-const CLIENTDEVPORT = 5173
+const SERVERDEVPORT = 4741;
+const CLIENTDEVPORT = 5173;
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL);
 
-const app = express()
+const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${CLIENTDEVPORT}` }))
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || `http://localhost:${CLIENTDEVPORT}`,
+  })
+);
 
-const PORT = process.env.PORT || SERVERDEVPORT
+const PORT = process.env.PORT || SERVERDEVPORT;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/users', userRouter)
+app.use('/users', userRouter);
+
+app.use('/users', require('./routes/users'));
+
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/items', ensureLoggedIn, require('./routes/items'));
+app.use('/orders', ensureLoggedIn, require('./routes/orders'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log('listening on port ' + PORT)
-})
+  console.log('listening on port ' + PORT);
+});
 
-module.exports = app
+module.exports = app;
